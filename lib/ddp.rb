a6db6ef9bb63d9331aeb4d1548aa8f52b265ad98
hisaichi5518 <hisaichi5518@gmail.com>
2014-11-05 13:55:25 +0900

    Added dumper and p

--- lib/ddp.rb
+++ lib/ddp.rb
@@ -1,5 +1,22 @@
 require "ddp/version"
 
 module Ddp
-  # Your code goes here...
+  def self.p(obj)
+    puts dumper(obj)
+  end
+
+  def self.dumper(obj)
+    text = <<-EOF
+#{((obj.class.ancestors - obj.class.included_modules).join(" < "))} {
+  included_modules:
+    #{obj.class.included_modules.join("\n    ")}
+  inspect:
+    #{obj.inspect}
+    EOF
+
+    if obj.methods.include?(:source_location)
+      text += "  source_location: #{obj.source_location || "(Opps! this is a C method.)"}\n"
+    end
+    text += "}"
+  end
 end
--- spec/ddp_spec.rb
+++ spec/ddp_spec.rb
@@ -1,11 +1,51 @@
 require 'spec_helper'
+require "pp"
 
 describe Ddp do
   it 'has a version number' do
     expect(Ddp::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "#dumper" do
+    context "arguments is Hash Object" do
+      it 'returns pretty string' do
+        result = Ddp.dumper({test: "test"})
+        expect(result).to match /Hash < Object < BasicObject {/
+        expect(result).to match /included_modules:/
+        expect(result).to match /inspect:/
+        expect(result).not_to match /source_location:/
+      end
+    end
+
+    context "arguments is Class Object" do
+      it 'returns pretty string' do
+        result = Ddp.dumper(self)
+        expect(result).to match /Object < BasicObject {/
+        expect(result).to match /included_modules:/
+        expect(result).to match /inspect:/
+        expect(result).not_to match /source_location:/
+      end
+    end
+
+    context "arguments is Method Object" do
+      it 'returns pretty string' do
+        result = Ddp.dumper(Ddp.method(:p))
+        expect(result).to match /Object < BasicObject {/
+        expect(result).to match /included_modules:/
+        expect(result).to match /inspect:/
+        expect(result).to match /source_location:/
+      end
+    end
+
+    context "arguments is C method Object" do
+      it 'returns pretty string' do
+        result = Ddp.dumper(100000.method(:abs))
+        expect(result).to match /Object < BasicObject {/
+        expect(result).to match /included_modules:/
+        expect(result).to match /inspect:/
+        expect(result).to match /source_location:/
+        expect(result).to match /C method/
+      end
+    end
   end
 end
