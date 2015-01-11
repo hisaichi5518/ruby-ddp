47d2ffa50c72943c23f3fcf2bc12c9148083e904
hisaichi5518 <hisaichi5518@gmail.com>
2015-01-11 11:14:38 +0900

    removed tests

--- spec/ddp_spec.rb
+++ spec/ddp_spec.rb
@@ -5,46 +5,4 @@ describe Ddp do
     expect(Ddp::VERSION).not_to be nil
   end
 
-  describe "#dumper" do
-    context "arguments is Hash Object" do
-      it 'returns pretty string' do
-        result = Ddp.dumper({test: "test"})
-        expect(result).to match /Hash < Object < BasicObject {/
-        expect(result).to match /included_modules:/
-        expect(result).to match /inspect:/
-        expect(result).not_to match /source_location:/
-      end
-    end
-
-    context "arguments is Class Object" do
-      it 'returns pretty string' do
-        result = Ddp.dumper(self)
-        expect(result).to match /Object < BasicObject {/
-        expect(result).to match /included_modules:/
-        expect(result).to match /inspect:/
-        expect(result).not_to match /source_location:/
-      end
-    end
-
-    context "arguments is Method Object" do
-      it 'returns pretty string' do
-        result = Ddp.dumper(Ddp.method(:p))
-        expect(result).to match /Object < BasicObject {/
-        expect(result).to match /included_modules:/
-        expect(result).to match /inspect:/
-        expect(result).to match /source_location:/
-      end
-    end
-
-    context "arguments is C method Object" do
-      it 'returns pretty string' do
-        result = Ddp.dumper(100000.method(:abs))
-        expect(result).to match /Object < BasicObject {/
-        expect(result).to match /included_modules:/
-        expect(result).to match /inspect:/
-        expect(result).to match /source_location:/
-        expect(result).to match /C method/
-      end
-    end
-  end
 end
